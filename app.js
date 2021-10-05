f8ae887646655db3b874abba75e75983cd6058d8
david3335 <sh330035@gmail.com>
2021-10-05 13:14:13 +0800

    feat. pages setting & CSS setting

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 // module import
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 
 // variation assign
 const port = 3000
 
+// template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// setting static files
+app.use(express.static('public'))
+
 // routing
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
 })
 
 // server listener
